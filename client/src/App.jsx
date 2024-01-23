9bfc9b21f24ec92c9a7c45b62cc13e89f3a3f13b
MohiniMehta <mohinimehta12@gmail.com>
2024-01-23 13:49:02 +0530

    connected frontend to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
-
+import { useEffect, useState} from "react";
 
 export default function App() {
+   const [message,setMessage]=useState("");
+  useEffect(() => {
+    async function getTodos() {
+      try {
+        const res = await fetch("/api/todos");
+        if (!res.ok) {
+          throw new Error(`Failed to fetch data: ${res.status}`);
+        }
+        const todos = await res.json();
+    
+        console.log(todos);
+        setMessage(todos.mssg);
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    }
+    getTodos();
+  }, []);
+  
+
   return (
    <main className="container">
     <h1>TO DO's</h1>
+    {message &&<p>{message}</p>}
    </main>
   );
 }
