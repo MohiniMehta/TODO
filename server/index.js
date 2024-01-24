e5efbb7b9b30d55d9687cda9e3419fb0c6e51d24
MohiniMehta <mohinimehta12@gmail.com>
2024-01-24 11:06:41 +0530

    backend ready

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const {connectToMongoDB}=require("./database");
 const app=express();
+app.use(express.json());
 const router=require("./routes");
 app.use("/api",router)
 const port=process.env.PORT || 5000;
